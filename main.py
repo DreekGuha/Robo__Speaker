b4737c13938a010fa26376c12ed3d147e7978198
Dreek Guha <dreekguha@gmail.com>
2023-04-21 16:33:58 +0530

    Added some changes (if __name__ == '__main__':)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 
-print("Welcome to my Robo Speaker")
-while True:
-    txt=input("Enter what you want me to speak\n")
-    if(txt=="q"):
-        os.system(f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Thank you,Have a nice day')")
-        break
-    command= f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{txt}')"
-    os.system(command)
+if __name__ == '__main__':
+
+    print("Welcome to my Robo Speaker")
+    while True:
+        txt=input("Enter what you want me to speak\n")
+        if(txt=="q"):
+            os.system(f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Thank you,Have a nice day')")
+            break
+        command= f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{txt}')"
+        os.system(command)
