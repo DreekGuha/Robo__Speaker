65e67c6488b3766e82fc589b6ad7714d3cfb0ad9
Dreek Guha <dreekguha@gmail.com>
2023-04-21 16:30:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+print("Welcome to my Robo Speaker")
+while True:
+    txt=input("Enter what you want me to speak\n")
+    if(txt=="q"):
+        os.system(f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('Thank you,Have a nice day')")
+        break
+    command= f"Powershell Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{txt}')"
+    os.system(command)
